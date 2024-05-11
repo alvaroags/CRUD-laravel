37235a7c445752ea07bd8c9a8238b071a610cf70
Álvaro Gomes <95548708+alvaroags@users.noreply.github.com>
2024-05-11 10:44:18 -0300

    :sparkles: Criação da tabela de tarefas

---
+++ database/migrations/2024_05_11_021106_create_model_tarefas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->increments('id');
+            // $table->integer('user_id')->unsigned();
+            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('tarefa');
+            $table->text('descricao')->nullable();
+            $table->enum('status', ['Pendente', 'Em andamento', 'Concluída'])->default('pendente');
+            $table->enum('prioridade', ['Baixa', 'Média', 'Alta'])->default('baixa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tarefas');
+    }
+};

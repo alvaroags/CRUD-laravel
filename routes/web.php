3c965954a0c8551955e215791f7711e99547bece
Álvaro Gomes <95548708+alvaroags@users.noreply.github.com>
2024-05-11 10:46:43 -0300

    :sparkles: Criação das rotas
    Foram Criadas as rotas do index, create, show e store

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TarefaController;
+// use app\Http\Controllers\TarefaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/tarefa', [TarefaController::class, 'index']);
+Route::get('/tarefa/create', [TarefaController::class, 'create']);
+Route::get('/tarefa/{id}', [TarefaController::class, 'show']);
+Route::post('/tarefa', [TarefaController::class, 'store']);
+

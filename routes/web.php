2a09726643a3bc8fd823df0edd8c8eef07e4a66e
Álvaro Gomes <95548708+alvaroags@users.noreply.github.com>
2024-05-14 00:12:42 -0300

    :sparkles: Criação de Novas Rotas

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TarefaController;
 // use app\Http\Controllers\TarefaController;
 
+Route::delete('/tarefa/{id}', [TarefaController::class, 'destroy']);
 Route::get('/tarefa', [TarefaController::class, 'index']);
 Route::get('/tarefa/create', [TarefaController::class, 'create']);
-Route::get('/tarefa/{id}', [TarefaController::class, 'show']);
+// Route::get('/tarefa/{id}', [TarefaController::class, 'show']);
 Route::post('/tarefa', [TarefaController::class, 'store']);
-
+Route::get('/tarefa/{id}/edit', [TarefaController::class, 'edit']);
+Route::put('/tarefa/{id}', [TarefaController::class, 'update']);
+Route::put('/tarefa/{id}/status', [TarefaController::class, 'updateStatus']);
+Route::put('/tarefa/{id}/prioridade', [TarefaController::class, 'updatePrioridade']);
